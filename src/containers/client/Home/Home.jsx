8f0c89cdfe7dea51655a27c858d6726281a4d433
NhatNamVo <nhatnamvstt@gmail.com>
2021-09-05 00:44:50 +0700

    create layout list of showing film and hot film

--- src/containers/client/Home/Home.jsx
+++ src/containers/client/Home/Home.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import MovieList from './MovieList/MovieList';
 import Carousel from './Carousel/Carousel';
 import Filter from './Filter/Filter';
+import Movieshow from './Movieshow/Movieshow';
+import Moviehot from './Moviehot/Movieshow';
 export default class Home extends Component {
   render() {
-    console.log(this.props)
     return (
       <div>
         <Carousel match = {this.props.match} history={this.props.history} location={this.props.location}/>
-        {/* <Filter /> */}
+        <Movieshow/>
+        <Moviehot/>
       </div>
     );
   }
