258ad2096b99fbff29a64d3b9745827c60167f71
NhatNamVo <nhatnamvstt@gmail.com>
2021-09-20 23:25:27 +0700

    update seatPlan

---
+++ src/containers/shared/UserInfo/UserInfo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserInfo extends Component {
+    render() {
+        return (
+            <div>
+                UserInfo- Ticket Checking
+            </div>
+        );
+    }
+}
+
+export default UserInfo;
