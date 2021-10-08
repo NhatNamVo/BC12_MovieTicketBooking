a66e1f26d9464953876567fdd0bdfad3671ea4fa
LinhTrang <83943885+chanchang24@users.noreply.github.com>
2021-10-09 02:11:08 +0700

    layout header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,73 +2,83 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import UserLogin from "./UserLogin/UserLogin";
+import "./Header.scss";
 class Header extends Component {
   checkLogin = () => {
     if (!!this.props.currentUser) {
       return (
-        <li className="nav-item"><UserLogin /></li>
+        <li className="nav-item header-user">
+          <UserLogin />
+        </li>
       );
     } else {
       return (
         <>
-          <li className="nav-item">
+          <div className="   pr-0 header-button">
             <Link className="nav-link" to="/login">
               Đăng nhập
             </Link>
-          </li>
-          <li className="nav-item">
+          </div>
+          {/* <li className="nav-item">
             <Link className="nav-link" to="/register">
               Đăng ký
             </Link>
-          </li>
+          </li> */}
         </>
       );
     }
   };
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          Cinema Booking
-        </Link>
-        <button
-          className="navbar-toggler d-lg-none"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavId"
-          aria-controls="collapsibleNavId"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/movie">
-                Movies
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/theater">
-                Theaters
-              </Link>
-            </li>
+      <div className="header-section">
+        <div className="container">
+          <nav className="navbar navbar-expand-sm">
+            <Link className="navbar-brand" to="/">
+              Cinema Booking
+            </Link>
+            <button
+              className="navbar-toggler d-lg-none"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapsibleNavId"
+              aria-controls="collapsibleNavId"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i class="fa fa-minus"></i>
+            </button>
+            <div
+              className="collapse  navbar-collapse header__menu "
+              id="collapsibleNavId"
+            >
+              <ul className="navbar-nav  mt-2 mt-lg-0 menu">
+                <li className="nav-item active ">
+                  <Link className="nav-link " to="/">
+                    Trang chủ
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/movie">
+                    Danh sách phim
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/theater">
+                    Cụm rạp
+                  </Link>
+                </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/admin">
-                Contact
-              </Link>
-            </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/admin">
+                    Liên hệ
+                  </Link>
+                </li>
+              </ul>
+            </div>
             {this.checkLogin()}
-          </ul>
+          </nav>
         </div>
-      </nav>
+      </div>
     );
   }
 }
