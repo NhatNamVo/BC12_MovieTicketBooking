9f8d69359c1d4d3b6c0caa5fa25d4dd43d8559f0
NhatNamVo <nhatnamvstt@gmail.com>
2021-10-10 17:13:31 +0700

    completed header client component responsive

--- src/containers/client/Client.jsx
+++ src/containers/client/Client.jsx
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import ModalTrailer from "containers/shared/ModalTrailer/ModalTrailer";
 import PageNotFound from "containers/shared/PageNotFound/PageNotFound";
 import ClientLayout from "layouts/ClientLayout";
+import './Client.scss';
 class Client extends Component {
   renderLayout = (routes, Layout) => {
     return routes.map((route) => {
@@ -16,7 +17,7 @@ class Client extends Component {
     const background =
       this.props.location.state && this.props.location.state.background;
     return (
-      <div>
+      <div className="movieTicket-content">
         <Switch location={background || this.props.location}>
           {this.renderLayout(clientRoutes, ClientLayout)}
           <Route path="*" component={PageNotFound} />
